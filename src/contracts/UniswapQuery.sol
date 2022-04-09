c11b8a1b79748455eb78bbf7330af2d75fd61228
Alexander <alexanderholmberg91@gmail.com>
2022-04-09 09:07:43 +0200

    basic contract for getting current reserves

---
+++ src/contracts/UniswapQuery.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+interface IUniswapV2Pair {
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+}
+
+contract UniswapQuery {
+    function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
+        uint256[3][] memory result = new uint256[3][](_pairs.length);
+        for (uint i = 0; i < _pairs.length; i++) {
+            (result[i][0], result[i][1], result[i][2]) = _pairs[i].getReserves();
+        }
+        return result;
+    }
+}
