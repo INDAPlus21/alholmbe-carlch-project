af233ec75e62284f02ac839ed0d29f4ce4ced176
Alexander <alexanderholmberg91@gmail.com>
2022-04-13 21:29:44 +0200

    get pairs in batches and update reserves on every new block

--- src/abi.ts
+++ src/abi.ts
@@ -1,27 +1,54 @@
 export const UNISWAP_QUERY_ABI = [
     {
+        inputs: [],
+        name: 'BadIndex',
+        type: 'error',
+    },
+    {
         inputs: [
             {
                 internalType: 'contract UniswapV2Factory',
                 name: '_uniswapFactory',
                 type: 'address',
             },
-            { internalType: 'uint256', name: '_start', type: 'uint256' },
             {
                 internalType: 'uint256',
-                name: '_stop',
+                name: '_startIndex',
+                type: 'uint256',
+            },
+            {
+                internalType: 'uint256',
+                name: '_stopIndex',
                 type: 'uint256',
             },
         ],
-        name: 'getPairsByIndexRange',
-        outputs: [{ internalType: 'address[3][]', name: '', type: 'address[3][]' }],
+        name: 'getPairsByRange',
+        outputs: [
+            {
+                internalType: 'address[3][]',
+                name: '',
+                type: 'address[3][]',
+            },
+        ],
         stateMutability: 'view',
         type: 'function',
     },
     {
-        inputs: [{ internalType: 'contract IUniswapV2Pair[]', name: '_pairs', type: 'address[]' }],
+        inputs: [
+            {
+                internalType: 'contract IUniswapV2Pair[]',
+                name: '_pairs',
+                type: 'address[]',
+            },
+        ],
         name: 'getReservesByPairs',
-        outputs: [{ internalType: 'uint256[3][]', name: '', type: 'uint256[3][]' }],
+        outputs: [
+            {
+                internalType: 'uint256[3][]',
+                name: '',
+                type: 'uint256[3][]',
+            },
+        ],
         stateMutability: 'view',
         type: 'function',
     },
--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,19 @@ const ETHEREUM_RPC_URL = 'https://bsc-dataseed.binance.org/';
 const provider = new providers.StaticJsonRpcProvider(ETHEREUM_RPC_URL);
 
 const main = async () => {
+    console.log('main');
+
     // a market is two different tokens that are tradeable, for example ETH/USDC
     // get ALL markets that exists on the factory addresses (which are exchanges)
     const markets = await UniswapV2EthPair.getUniswapMarketsByToken(provider, FACTORY_ADDRESSES);
+    console.log('Number of pairs:', markets.allMarketPairs.length);
+
+    provider.on('block', async (blockNumber) => {
+        console.log('block', blockNumber);
+
+        // on every new block, update reserves
+        await UniswapV2EthPair.updateReserves(provider, markets.allMarketPairs);
+    });
 };
 
 main();
