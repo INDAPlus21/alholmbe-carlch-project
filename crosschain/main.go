abe6ac354d3ec01254134b884cf21948e3481c44
Alexander <alexanderholmberg91@gmail.com>
2022-04-25 16:01:29 +0200

    add market type

--- crosschain/main.go
+++ crosschain/main.go
@@ -1,86 +1,89 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+	"sync"
 
-  "fmt"
-  "log"
-  // "os"
-  // "bufio"
-  "context"
-
-  "math/big"
-  // "encoding/json"
-
-  // "github.com/ethereum/go-ethereum/common"
-  "github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type Pair struct {
-  Market    string    `json:"_marketAddress"`
-  Tokens    []string  `json:"_tokens"`
+	Market string   `json:"_marketAddress"`
+	Tokens []string `json:"_tokens"`
 }
 
-const marketPairs = "src/randomjson/bsc_allMarketPairs.json"
+// const ethereum = "https://cloudflare-eth.com"
+const ethereum_archival = "https://eth-mainnet.alchemyapi.io/v2/u-P4WTWXjk4duuq6e4VO8aygIYiSJl4H"
+const avalanche = "https://rpc.ankr.com/avalanche"
+const bsc = "https://bsc-dataseed.binance.org/"
+const polygon = "https://polygon-rpc.com/"
+const aurora = "https://mainnet.aurora.dev"
+
+type Market struct {
+	protocol      string         // ex "ethereum"
+	marketAddress string         // ex "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc"
+	tokenBalances map[string]int // maps address to balance
+}
 
 func main() {
-  chains := [7]string{
-    "https://rpc3.fantom.network",
-    "https://bsc-dataseed.binance.org/",
-    "https://cloudflare-eth.com",
-    "https://polygon-rpc.com/",
-    "https://rpc.heavenswail.one",
-    "https://mainnet.aurora.dev",
-    "https://mainnet.optimism.io",
-  }
-  /*
-  file_json, err := os.Open(marketPairs)
-  if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Printf("Successfully opened file\n")
+	chains := [5]string{
+		ethereum_archival,
+		avalanche,
+		bsc,
+		polygon,
+		aurora,
+	}
 
-  r := bufio.NewReader(file_json)
-  d := json.NewDecoder(r)
-  i := 0
+	wg := new(sync.WaitGroup)
+	wg.Add(len(chains))
 
-  d.Token()
-  for d.More() {
-    pair := &Pair{}
-    d.Decode(pair)
-    fmt.Printf("%s\n", pair.Tokens[0])
-    i++
-  }
-  fmt.Printf("Successfully read %d objects\n", i)
+	// this should run until we terminate
+	for i := 0; i < 7; i++ {
+		go fetchAPI(chains[i], wg)
+	}
 
-  defer file_json.Close()
-  */
-  for i := 0; i < 7; i++ {
-    go fetchAPI(chains[i])
-  }
-  for { }
+	wg.Wait()
 }
 
-func fetchAPI(chain string) {
-  /* Dail to chain */
-  for {
-    client, err := ethclient.Dial(chain)
-    if err != nil { log.Fatal(err) }
-    fmt.Printf("Successfully connected to %s\n", chain)
+func fetchAPI(chain string, wg *sync.WaitGroup) {
 
-    /* Do stuff */
-    blockNumber := big.NewInt(0)
+	/* Dial to chain */
+	// get markets
 
-    for {
-      header, err := client.HeaderByNumber(context.Background(), nil)
-      if err != nil { break }
+	// listener that waits on the next block and updates the reserves
 
-      if blockNumber.Cmp(header.Number) == 0 { continue }
+	for {
+		client, err := ethclient.Dial(chain)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully connected to %s\n", chain)
 
-      blockNumber = header.Number
-      block, err := client.BlockByNumber(context.Background(), blockNumber)
-      if err != nil { break }
+		/* Do stuff */
+		blockNumber := big.NewInt(0)
 
-      fmt.Printf("%s\t : %d\t : %s\n", blockNumber, len(block.Transactions()), chain)
-    }
-  }
+		for {
+			header, err := client.HeaderByNumber(context.Background(), nil)
+			if err != nil {
+				break
+			}
+
+			if blockNumber.Cmp(header.Number) == 0 {
+				continue
+			}
+
+			blockNumber = header.Number
+			block, err := client.BlockByNumber(context.Background(), blockNumber)
+			if err != nil {
+				break
+			}
+
+			fmt.Printf("%s\t : %d\t : %s\n", blockNumber, len(block.Transactions()), chain)
+		}
+	}
+
+	wg.Done()
 }
