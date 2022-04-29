f4f7fc1d96c70567864640166dbdb36ffc1dd542
Chemnitz <carlch@kth.se>
2022-04-29 16:00:11 +0200

    Add selected token addresses

---
+++ crosschain/chain_interaction/token.go
@@ -0,0 +1,53 @@
+package main
+
+import ()
+
+/*
+ * Datastruktur för Token
+ * Kontrakt av en token typ ska ge alla andra tokens av samma typ
+ */
+
+// Token: BNB
+// ==========
+// Ether:     0xB8c77482e45F1F44dE1745F52C74426C631bDD52
+// Binance:   0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
+// Polygon:   0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3
+
+// Token: ETH
+// ==========
+// Ether:     0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+// Binance:   0x2170ed0880ac9a755fd29b2688956bd959f933f8
+// Fantom:    0x658b0c7613e890ee50b8c4bc6a3f41ef411208ad
+
+// Token: USDT
+// ===========
+// Ether:     0xdac17f958d2ee523a2206206994597c13d831ec7
+// Polygon:   0xc2132D05D31c914a87C6611C10748AEb04B58e8F
+// Avalanche: 0xc7198437980c041c805A1EDcbA50c1Ce5db95118
+
+// Token: USDC
+// ===========
+// Ether:     0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+// Binance:   0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d
+// Polygon:   0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
+// Avalanche: 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E
+// Fantom:    0x04068DA6C83AFCFA0e13ba15A6696662335D5B75
+
+// Token: BTC
+// ==========
+// Ether:     0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599
+// Polygon:   0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6
+// Avalanche: 0x50b7545627a5162F82A992c33b87aDc75187B218
+// Fantom:    0x321162Cd933E2Be498Cd2267a90534A804051b11
+
+// Token: UST
+// ==========
+// Ether:     0xa47c8bf37f92aBed4A126BDA807A7b7498661acD
+// Binance:   0x23396cF899Ca06c4472205fC903bDB4de249D6fC
+// Polygon:   0x692597b009d13C4049a947CAB2239b7d6517875F
+
+// Token: AVAX
+// ===========
+// Binance:   0x1CE0c2827e2eF14D5C4f29a091d735A204794041
+// Avalanche: 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7
+// Fantom:    0x511D35c52a3C244E7b8bd92c0C297755FbD89212
